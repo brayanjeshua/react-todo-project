8af183b370a8f036ac14a70a30ae5878821a8aca
Brayan Jeshua <brayanjeshuavz@gmail.com>
2023-04-03 10:36:55 +0200

    Fix wrong route

--- src/Router.js
+++ src/Router.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { HashRouter, Routes, Route } from "react-router-dom";
 import {HomePage} from './views/HomePage';
-import {CreatePage} from './views/EditPage';
+import {CreatePage} from './views/CreatePage';
 import {EditPage} from './views/EditPage';
 
 function Router() {
